66e9aa3b7ee2e3e218fdb918787ff4073d03c938
Mikita-Bahamazau-Leverx <mikita.bahamazau@leverx.com>
2022-07-22 00:32:41 +0300

    add basic Country table component

---
+++ src/components/CountryTable/CountryTable.tsx
@@ -0,0 +1,86 @@
+import "./countryTable.scss";
+
+import React, { useState } from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+const DEFAULT_CLASSNAME = "country-table";
+const CLASSNAMES = {
+  FLAG: `${DEFAULT_CLASSNAME}__flag`,
+};
+
+const columnsDefs: GridColDef[] = [
+  {
+    field: "flag",
+    headerName: "flag",
+    minWidth: 70,
+    maxWidth: 150,
+    flex: 0.5,
+    sortable: false,
+    renderCell: (params) => {
+      return <img className={CLASSNAMES.FLAG} src={params.row.flag} />;
+    },
+  },
+  {
+    field: "countryName",
+    headerName: "Country name",
+    minWidth: 100,
+    maxWidth: 450,
+    flex: 0.5,
+  },
+  {
+    field: "region",
+    headerName: "Region",
+    minWidth: 70,
+    maxWidth: 190,
+    flex: 0.4,
+    sortable: false,
+  },
+  {
+    field: "population",
+    headerName: "Population",
+    minWidth: 70,
+    maxWidth: 190,
+    flex: 0.4,
+  },
+  { field: "languages", headerName: "Languages", width: 130, sortable: false },
+  {
+    field: "currencies",
+    headerName: "Currencies",
+    minWidth: 90,
+    flex: 1,
+    sortable: false,
+    renderCell: (params) => {
+      return params.row.currencies?.map((currency: Record<string, any>) => {
+        return (
+          <p>
+            {currency.symbol}: {currency.name}
+          </p>
+        );
+      });
+    },
+  },
+];
+
+interface CountryTableProps {
+  rows: Partial<GridColDef>[];
+}
+
+const CountryTable = ({ rows }: CountryTableProps) => {
+  const [pageSize, setPageSize] = useState(10);
+
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columnsDefs}
+        pageSize={pageSize}
+        onPageSizeChange={(pageSize) => setPageSize(pageSize)}
+        rowsPerPageOptions={[10, 25, 50, 100]}
+        onRowClick={() => console.log("you've clicked on the row")}
+        disableColumnSelector
+      />
+    </div>
+  );
+};
+
+export default CountryTable;
