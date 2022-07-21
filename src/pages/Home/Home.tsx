b3adc65857998f728a4516e000014e442a648bf3
Mikita-Bahamazau-Leverx <mikita.bahamazau@leverx.com>
2022-07-22 00:33:07 +0300

    add Country table to the Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,26 @@
+import { GridColDef } from "@mui/x-data-grid";
 import React from "react";
+import CountryTable from "../../components/CountryTable";
+import { rows } from "../../constants/mockedData";
 
 const Home = () => {
+  const mappedRows = rows.map((row) => ({
+    flag: row.flag,
+    countryName: row.name,
+    region: row.region,
+    id: row.numericCode,
+    key: row.numericCode,
+    population: row.population,
+    languages: row.languages.map((language) => language.name),
+    currencies: row.currencies,
+  })) as Partial<GridColDef>[];
+
   return (
     <div className="main__wrapper wrapper">
-      <main className="main">Lorem ipsum dolor sit amet.</main>
+      <main className="main">
+        <CountryTable rows={mappedRows} />
+        Lorem ipsum dolor sit amet.
+      </main>
     </div>
   );
 };
