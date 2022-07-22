2d0622a8287c618d8360aa5521b5488f11fb88b9
Mikita-Bahamazau-Leverx <mikita.bahamazau@leverx.com>
2022-07-22 19:20:18 +0300

    change header and footer styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,42 @@
+import "./header.scss";
+
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+
+import classnames from "classnames";
 
 //@ts-ignore
 import logo from "../../assets/icons/logo.png";
 
-import "./header.scss";
+const DEFAULT_CLASSNAME = "app-header";
+const CLASSNAMES = {
+  WRAPPER: `${DEFAULT_CLASSNAME}__wrapper`,
+  LOGO: `${DEFAULT_CLASSNAME}__logo`,
+  LOGO_IMAGE: `${DEFAULT_CLASSNAME}__logo-image`,
+  NAVIGATION: `${DEFAULT_CLASSNAME}__nav`,
+  NAVIGATION_LIST: `${DEFAULT_CLASSNAME}__nav-list`,
+  NAVIGATION_ITEM: `${DEFAULT_CLASSNAME}__nav-item`,
+  NAVIGATION_LINK: `${DEFAULT_CLASSNAME}__nav-link`,
+};
 
 const Header = () => {
   return (
-    <div className="head__wrapper wrapper">
-      <header className="head">
-        <Link className="head__logo" to="/">
-          <img src={logo} alt="logo" className="head__logo__img" />
+    <div className={classnames(CLASSNAMES.WRAPPER, "wrapper")}>
+      <header className={DEFAULT_CLASSNAME}>
+        <Link className={CLASSNAMES.LOGO} to="/">
+          <img src={logo} alt="logo" className={CLASSNAMES.LOGO_IMAGE} />
         </Link>
-        <nav className="head__nav">
-          <ul className="nav__list">
-            <li className="nav__item">
-              <a className="nav__link"></a>
+        <nav className={CLASSNAMES.NAVIGATION}>
+          <ul className={CLASSNAMES.NAVIGATION_LIST}>
+            <li className={CLASSNAMES.NAVIGATION_ITEM}>
+              <NavLink to="/" exact className={CLASSNAMES.NAVIGATION_LINK}>
+                Home
+              </NavLink>
             </li>
-            <li className="nav__item">
-              <a className="nav__link"></a>
+            <li className={CLASSNAMES.NAVIGATION_ITEM}>
+              <NavLink to="/countries" className={CLASSNAMES.NAVIGATION_LINK}>
+                Countries list
+              </NavLink>
             </li>
           </ul>
         </nav>
