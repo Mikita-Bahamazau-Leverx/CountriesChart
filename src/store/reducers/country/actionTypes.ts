5706a1d0aef6093267e3673faf76774119ace3c9
Mikita-Bahamazau-Leverx <mikita.bahamazau@leverx.com>
2022-07-22 17:22:53 +0300

    add action types for filtered search

--- src/store/reducers/country/actionTypes.ts
+++ src/store/reducers/country/actionTypes.ts
@@ -4,6 +4,12 @@ export const getAllCountriesTypes = {
   error: "GET_ALL_COUNTRIES_FAILURE",
 };
 
+export const getCountriesTypes = {
+  start: "GET_COUNTRIES_REQUEST",
+  success: "GET_COUNTRIES_SUCCESS",
+  error: "GET_COUNTRIES_FAILURE",
+};
+
 export const getCountryTypes = {
   start: "GET_COUNTRY_REQUEST",
   success: "GET_COUNTRY_SUCCESS",
