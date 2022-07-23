2f4547a5edad9d6afad0615248fb68c08e14700c
Mikita-Bahamazau-Leverx <mikita.bahamazau@leverx.com>
2022-07-23 04:29:24 +0300

    remove search bar icon

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,8 +2,6 @@ import "./searchBar.scss";
 
 import React from "react";
 
-import IconButton from "@mui/material/IconButton";
-import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 
 interface SearchBarProps {
@@ -26,9 +24,6 @@ const SearchBar = ({ search, setSearchQuery }: SearchBarProps) => {
         placeholder="Search..."
         size="small"
       />
-      <IconButton type="submit" aria-label="search">
-        <SearchIcon style={{ fill: "blue" }} />
-      </IconButton>
     </form>
   );
 };
