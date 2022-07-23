9f10b554c7a37e5f108b816a2118cc30fbad6df3
Mikita-Bahamazau-Leverx <mikita.bahamazau@leverx.com>
2022-07-23 04:12:34 +0300

    add country chart to the main page

--- src/pages/Home/index.ts
+++ src/pages/Home/index.ts
@@ -1,3 +1,18 @@
+import { Dispatch } from "redux";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { ActionCreators, selectors } from "../../store/reducers/country";
+
+const selector = createStructuredSelector({
+  countries: selectors.countries,
+  isLoadingCountries: selectors.isLoadingCountries,
+});
+
 import Home from "./Home";
 
-export default Home;
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  getAllCountries: () => ActionCreators.getAllCountries()(dispatch),
+});
+
+export default connect(selector, mapDispatchToProps)(Home);
