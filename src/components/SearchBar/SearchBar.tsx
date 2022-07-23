1b76027a0bae8016b110355b750a581a9d49db89
Mikita-Bahamazau-Leverx <mikita.bahamazau@leverx.com>
2022-07-23 04:47:44 +0300

    make search bar normal sized

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -4,6 +4,11 @@ import React from "react";
 
 import TextField from "@mui/material/TextField";
 
+const DEFAULT_CLASSNAME = "search-bar";
+const CLASSNAMES = {
+  TEXT_FIELD: `${DEFAULT_CLASSNAME}__text-field`,
+};
+
 interface SearchBarProps {
   search: string;
   setSearchQuery: (value: string) => void;
@@ -14,7 +19,7 @@ const SearchBar = ({ search, setSearchQuery }: SearchBarProps) => {
     <form>
       <TextField
         id="search-bar"
-        className="text"
+        className={CLASSNAMES.TEXT_FIELD}
         value={search}
         onChange={(e) => {
           setSearchQuery(e.target.value);
@@ -22,7 +27,6 @@ const SearchBar = ({ search, setSearchQuery }: SearchBarProps) => {
         label="Enter your request"
         variant="outlined"
         placeholder="Search..."
-        size="small"
       />
     </form>
   );
