d9408a2790fecf94603476c69d4d1b608bc7e4f4
Mikita-Bahamazau-Leverx <mikita.bahamazau@leverx.com>
2022-07-23 03:06:40 +0300

    add chart component

---
+++ src/components/BarChart/DataChart.tsx
@@ -0,0 +1,48 @@
+import "./dataChart.scss";
+
+import React from "react";
+
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+interface AxisDefs {
+  x: string;
+  y: string;
+}
+
+interface DataChartProps {
+  data: Record<string, any>[];
+  axisDefs: AxisDefs;
+}
+
+const DataChart = ({ data, axisDefs }: DataChartProps) => {
+  return (
+    <BarChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey={axisDefs.x} />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey={axisDefs.y} fill="grey" />
+    </BarChart>
+  );
+};
+
+export default DataChart;
